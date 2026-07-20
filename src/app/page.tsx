da0c5810a820e26bbec844f5e3c3121c2fee3bec
Ap89-24 <aman082199@gmail.com>
2026-07-21 01:00:28 +0530

    types defined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 "use client";
 import InfiniteCarousel from "./components/InfiniteCarousel";
-import { projects } from "../data/projects";
-import LandingPage from "./components/uniqueBackground";
+import { Project, projects } from "../data/projects";
+// import LandingPage from "./components/uniqueBackground";
 
+interface ProjectsProps {
+  projects: Project[];
+  onHoverStart?: () => void;
+  onHoverEnd?: () => void;
+  play: () => void;
+  reverse: () => void;
+  kill: () => void;
+};
 
 export default function Home() {
-
+  const play = () => { };
+  const reverse = () => { };
+  const kill = () => { };
 
   return (
     <main className="relative h-screen w-full flex items-center overflow-hidden">
 
-      <LandingPage />
+      {/* <LandingPage /> */}
       <InfiniteCarousel
         projects={projects}
+        play={play}
+        reverse={reverse}
+        kill={kill}
       />
-   </main>
+    </main>
   );
 }
